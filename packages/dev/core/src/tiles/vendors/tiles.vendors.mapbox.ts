7bd6486c3b1f4f6468b9e39b7fd7fbf4aa0f9726
NathDotvision <nmorel@dotvision.com>
2023-12-04 18:22:09 +0100

    uptdate tiles in mapbox

--- packages/dev/core/src/tiles/vendors/tiles.vendors.mapbox.ts
+++ packages/dev/core/src/tiles/vendors/tiles.vendors.mapbox.ts
@@ -1,22 +1,47 @@
 import { Side } from "../../geometry/geometry.interfaces";
 import { DemTileWebClient } from "../../dem/dem.tileclient";
 import { TileWebClient, TileWebClientOptions } from "../tiles.client";
-import { Float32TileCodec, Float32TileCodecOptionsBuilder } from "../tiles.codecs.image";
+import { Float32TileCodec, Float32TileCodecOptionsBuilder, ImageTileCodec } from "../tiles.codecs.image";
 import { EPSG3857 } from "../tiles.geography";
 import { IPixelDecoder } from "../tiles.interfaces";
 import { TileMetricsOptionsBuilder } from "../tiles.metrics";
 import { WebTileUrlBuilder } from "../tiles.urlBuilder";
 
+export enum MapBoxMap2DLayerCode {
+    bathymetry = "mapbox.mapbox-bathymetry-v2",
+    country= "mapbox.country-boundaries-v1",
+    satellite = "mapbox.satellite",
+    tansit="mapbox.transit-v2",
+    terrain = "mapbox.mapbox-terrain-dem-v1",
+    terrain_rgb= "mapbox.terrain-rgb",
+    terrainv2= "mapbox.mapbox-terrain-v2",
+    traffic= "mapbox.mapbox-traffic-v1",
+}
+
 export class MapBoxTerrainDemV1UrlBuilder extends WebTileUrlBuilder {
     // https://api.mapbox.com/raster/v1/mapbox.mapbox-terrain-dem-v1/14/8800/5372.webp?sku=101iNLHSEcgVj&access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY5YzJzczA2ejIzM29hNGQ3emFsMXgifQ.az9JUrQP7klCgD3W-ueILQ
-    public constructor(token: string, extension = "webp") {
+
+    private static TOKEN = "pk.eyJ1IjoiY2xvbmdlYW5pZSIsImEiOiJjajZ3YWJ3bTQxcDk5Mnhxc29lbzMzdm54In0.-hY_qdTaIeZcZlRivs947Q";
+
+    public static Bathymetry = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.bathymetry);
+    public static Country = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.country);
+    public static Satellite = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.satellite);
+    public static Terrain = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.terrain);
+    public static TerrainRGB = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.terrain_rgb);
+    public static TerrainV2 = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.terrainv2);
+    public static Traffic = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.traffic);
+    public static Transit = new MapBoxTerrainDemV1UrlBuilder(MapBoxTerrainDemV1UrlBuilder.TOKEN, "webp", MapBoxMap2DLayerCode.tansit);
+
+    public constructor(token: string, extension = "webp", ...types: (MapBoxMap2DLayerCode | string)[]) {
         super();
         this.withHost("api.mapbox.com")
             .withSecure(true)
             .withQuery(`access_token=${token}`)
-            .withPath(`raster/v1/mapbox.mapbox-terrain-dem-v1/{z}/{x}/{y}{@2x}.{extension}`)
+            .withPath(`v4/${types}/{z}/{x}/{y}@2x.{extension}90?`)
             .withExtension(extension);
     }
+    //FIXME : add token in parameter and use it in constructor
+    //FIXME : regarder pourquoi les images sont générer bizzarement
 }
 
 export class MapboxAltitudeDecoder implements IPixelDecoder {
@@ -34,19 +59,48 @@ export class MapboxAltitudeDecoder implements IPixelDecoder {
 
 export class MapBox {
     private static readonly KEY = "mapbox";
+
     public static MaxLevelOfDetail = 14;
+    public static MetricsOptions = new TileMetricsOptionsBuilder().withTileSize(512).withMaxLOD(MapBox.MaxLevelOfDetail).build();
+    public static Metrics = new EPSG3857(MapBox.MetricsOptions);
 
     public static TerrainDemV1Client(token: string, options?: TileWebClientOptions) {
-        let mo = new TileMetricsOptionsBuilder().withTileSize(512).withMaxLOD(MapBox.MaxLevelOfDetail);
-        const metrics = new EPSG3857(mo.build());
         const codecOptions = new Float32TileCodecOptionsBuilder().withInsets(1, Side.top).withInsets(1, Side.left).withInsets(1, Side.bottom).withInsets(1, Side.right).build();
         const elevationClient = new TileWebClient(
             `${MapBox.KEY}_elevation`,
-            new MapBoxTerrainDemV1UrlBuilder(token),
+            new MapBoxTerrainDemV1UrlBuilder(token, "webp", MapBoxMap2DLayerCode.terrain),
             new Float32TileCodec(MapboxAltitudeDecoder.Shared, codecOptions),
-            metrics,
+            MapBox.Metrics,
             options
         );
         return new DemTileWebClient(`${MapBox.KEY}_dem`, elevationClient);
+    }
+
+    public static TerrainClientBathymetry(token : string, options?: TileWebClientOptions) {
+        return new TileWebClient(`${MapBox.KEY}_bathymetry`, MapBoxTerrainDemV1UrlBuilder.Bathymetry, new ImageTileCodec(), MapBox.Metrics, options);
+    }
+
+    public static TerrainClientCountry(token : string, options?: TileWebClientOptions) {
+        return new TileWebClient(`${MapBox.KEY}_country`, MapBoxTerrainDemV1UrlBuilder.Country, new ImageTileCodec(), MapBox.Metrics, options);
+    }
+
+    public static TerrainClientSatellite(token : string, options?: TileWebClientOptions) {
+        return new TileWebClient(`${MapBox.KEY}_satellite`, MapBoxTerrainDemV1UrlBuilder.Satellite, new ImageTileCodec(), MapBox.Metrics, options);
+    }
+
+    public static TerrainClientTerrain(token : string, options?: TileWebClientOptions) {
+        return new TileWebClient(`${MapBox.KEY}_terrain`, MapBoxTerrainDemV1UrlBuilder.Terrain, new ImageTileCodec(), MapBox.Metrics, options);
+    }
+
+    public static TerrainClientTerrainRGB(token : string, options?: TileWebClientOptions) {
+        return new TileWebClient(`${MapBox.KEY}_terrain_rgb`, MapBoxTerrainDemV1UrlBuilder.TerrainRGB, new ImageTileCodec(), MapBox.Metrics, options);
+    }
+
+    public static TerrainClientTerrainV2(token : string, options?: TileWebClientOptions) {
+        return new TileWebClient(`${MapBox.KEY}_terrain_v2`, MapBoxTerrainDemV1UrlBuilder.TerrainV2, new ImageTileCodec(), MapBox.Metrics, options);
+    }
+
+    public static TerrainClientTraffic(token : string, options?: TileWebClientOptions) {
+        return new TileWebClient(`${MapBox.KEY}_traffic`, MapBoxTerrainDemV1UrlBuilder.Traffic, new ImageTileCodec(), MapBox.Metrics, options);
     }
 }
--- packages/dev/core/types/tiles/vendors/tiles.vendors.mapbox.d.ts
+++ packages/dev/core/types/tiles/vendors/tiles.vendors.mapbox.d.ts
@@ -1,9 +1,29 @@
 import { DemTileWebClient } from "../../dem/dem.tileclient";
-import { TileWebClientOptions } from "../tiles.client";
+import { TileWebClient, TileWebClientOptions } from "../tiles.client";
+import { EPSG3857 } from "../tiles.geography";
 import { IPixelDecoder } from "../tiles.interfaces";
 import { WebTileUrlBuilder } from "../tiles.urlBuilder";
+export declare enum MapBoxMap2DLayerCode {
+    bathymetry = "mapbox.mapbox-bathymetry-v2",
+    country = "mapbox.country-boundaries-v1",
+    satellite = "mapbox.satellite",
+    tansit = "mapbox.transit-v2",
+    terrain = "mapbox.mapbox-terrain-dem-v1",
+    terrain_rgb = "mapbox.terrain-rgb",
+    terrainv2 = "mapbox.mapbox-terrain-v2",
+    traffic = "mapbox.mapbox-traffic-v1"
+}
 export declare class MapBoxTerrainDemV1UrlBuilder extends WebTileUrlBuilder {
-    constructor(token: string, extension?: string);
+    private static TOKEN;
+    static Bathymetry: MapBoxTerrainDemV1UrlBuilder;
+    static Country: MapBoxTerrainDemV1UrlBuilder;
+    static Satellite: MapBoxTerrainDemV1UrlBuilder;
+    static Terrain: MapBoxTerrainDemV1UrlBuilder;
+    static TerrainRGB: MapBoxTerrainDemV1UrlBuilder;
+    static TerrainV2: MapBoxTerrainDemV1UrlBuilder;
+    static Traffic: MapBoxTerrainDemV1UrlBuilder;
+    static Transit: MapBoxTerrainDemV1UrlBuilder;
+    constructor(token: string, extension?: string, ...types: (MapBoxMap2DLayerCode | string)[]);
 }
 export declare class MapboxAltitudeDecoder implements IPixelDecoder {
     static Shared: MapboxAltitudeDecoder;
@@ -12,5 +32,14 @@ export declare class MapboxAltitudeDecoder implements IPixelDecoder {
 export declare class MapBox {
     private static readonly KEY;
     static MaxLevelOfDetail: number;
+    static MetricsOptions: import("../tiles.interfaces").ITileMetricsOptions;
+    static Metrics: EPSG3857;
     static TerrainDemV1Client(token: string, options?: TileWebClientOptions): DemTileWebClient;
+    static TerrainClientBathymetry(token: string, options?: TileWebClientOptions): TileWebClient<HTMLImageElement>;
+    static TerrainClientCountry(token: string, options?: TileWebClientOptions): TileWebClient<HTMLImageElement>;
+    static TerrainClientSatellite(token: string, options?: TileWebClientOptions): TileWebClient<HTMLImageElement>;
+    static TerrainClientTerrain(token: string, options?: TileWebClientOptions): TileWebClient<HTMLImageElement>;
+    static TerrainClientTerrainRGB(token: string, options?: TileWebClientOptions): TileWebClient<HTMLImageElement>;
+    static TerrainClientTerrainV2(token: string, options?: TileWebClientOptions): TileWebClient<HTMLImageElement>;
+    static TerrainClientTraffic(token: string, options?: TileWebClientOptions): TileWebClient<HTMLImageElement>;
 }

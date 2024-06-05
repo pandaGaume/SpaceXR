1c8063665605f057c8f6239023ab04d360dede05
Guillaume Pelletier <guillaume.pelletier@dotvision.com>
2024-06-05 15:43:56 +0200

    fix virtual display with normals

--- packages/dev/babylon/src/display/display.virtual.ts
+++ packages/dev/babylon/src/display/display.virtual.ts
@@ -77,7 +77,9 @@ export class VirtualDisplay extends Mesh implements ITileDisplayBounds {
 
         data.positions = [0.5 * sx, 0.5 * sy, 0, -0.5 * sx, 0.5 * sy, 0, -0.5 * sx, -0.5 * sy, 0, 0.5 * sx, -0.5 * sy, 0];
         data.indices = [2, 0, 3, 0, 2, 1];
+        data.normals = [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1];
         data.uvs = [0, 0, 1, 0, 1, 1, 0, 1];
+
         return data;
     }
 
